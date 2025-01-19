cc704f2e6c93ab651533fdd16dcbf927e2c4581a
electric663hair <electric663hair@gmail.com>
2025-01-19 21:50:37 +0100

    Basic styling finished

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 const navButtons = document.querySelectorAll("nav .navButton");
-const characters = document.querySelector("#characters");
 const alphabet = document.querySelector(".alphabet");
 const simulator = document.querySelector(".simulator");
 const keyButton = document.querySelector("#telegraphKeyContainer");
 let startTime;
+let spaceDown;
 
 keyButton.addEventListener("mousedown", function() {
     startTime = performance.now();
     keyButton.classList.add("up")
 })
 keyButton.addEventListener("mouseleave", function() {
-    console.log(performance.now() - startTime)
-    keyButton.classList.remove("up")
+    if (keyButton.classList.contains("up")) {
+        console.log(performance.now() - startTime)
+        keyButton.classList.remove("up")
+    }
 })
 keyButton.addEventListener("mouseup", function() {
     console.log(performance.now() - startTime)
@@ -19,12 +21,18 @@ keyButton.addEventListener("mouseup", function() {
 })
 document.addEventListener("keydown", function(event) {
     if (event.code == "Space") {
-        startTime = performance.now()
-        keyButton.classList.add("up")
+        event.preventDefault();
+        
+        if ( !spaceDown) {
+            spaceDown = true;
+            startTime = performance.now()
+            keyButton.classList.add("up")
+        }
     }
 })
 document.addEventListener("keyup", function(event) {
     if (event.code == "Space") {
+        spaceDown = false;
         console.log(performance.now() - startTime)
         keyButton.classList.remove("up")
     }
@@ -43,12 +51,20 @@ navButtons.forEach(button => {
     })
 })
 
-function appendCharacters(element) {
-    charactersToAppend = ["A => .-", "B => -...", "C => -.-.", "D => -..", "E => .", "F => ..-.", "G => --.", "H => ....", "I => ..", "J => .---", "K => -.-", "L => .-..", "M => --", "N => -.", "O => ---", "P => .--.", "Q => --.-", "R => .-.", "S => ...", "T => -", "U => ..-", "V => ...-", "W => .--", "X => -..-", "Y => -.--", "Z => --.."]
+function appendCharacters(element, type) {
+    if (type == "l") charactersToAppend = ["A | .-", "B | -...", "C | -.-.", "D | -..", "E | .", "F | ..-.", "G | --.", "H | ....", "I | ..", "J | .---", "K | -.-", "L | .-..", "M | --", "N | -.", "O | ---", "P | .--.", "Q | --.-", "R | .-.", "S | ...", "T | -", "U | ..-", "V | ...-", "W | .--", "X | -..-", "Y | -.--", "Z | --.."]
+    else if (type == "n") charactersToAppend = ["1 | .----", "2 | ..---", "3 | ...--", "4 | ....-", "5 | .....", "6 | -....", "7 | --...", "8 | ---..", "9 | ----."]
+    else if (type == "s") charactersToAppend = [". | .-.-.-", ", | --..--", "? | ..--..", "! | -.-.--", "/ | -..-.", "( | -.--.", ") | -.--.-", "& | .-...", ": | ---...", "; | -.-.-.", "= | -...-", "+ | .-.-.", "- | -....-", "_ | ..--.-", "\" | .-..-.", "$ | ...-..-", "@ | .--.-.", "Æ | .-.-", "Ø | ---.", "Å | .--.-"]
 
-
-    charactersToAppend.forEach(character => {
-        element.insertAdjacentHTML("beforeend", `<div class="character">${character}</div>`)
+    element.forEach(__element => {
+        charactersToAppend.forEach(character => {
+            character = character.split(" | ")
+            console.log(character)
+            __element.insertAdjacentHTML("beforeend", `<div class="character"><span class="characterText">${character[0]}</span><span class="morseText">${character[1]}</span></div>`)
+        })
     })
 }
-appendCharacters(characters)
+
+appendCharacters(document.querySelectorAll(".letters"), "l")
+appendCharacters(document.querySelectorAll(".numbers"), "n")
+appendCharacters(document.querySelectorAll(".symbols"), "s")
