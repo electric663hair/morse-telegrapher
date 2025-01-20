eef704f9d2861d1c40ae1f88697fb4e45f50fe19
electric663hair <electric663hair@gmail.com>
2025-01-20 21:11:58 +0100

    Made morse chat display what you write

--- script.js
+++ script.js
@@ -1,24 +1,67 @@
 const navButtons = document.querySelectorAll("nav .navButton");
 const alphabet = document.querySelector(".alphabet");
 const simulator = document.querySelector(".simulator");
-const keyButton = document.querySelector("#telegraphKeyContainer");
+const keyButton = document.querySelector("#telegraphKeyButton");
 const copyButtons = document.querySelectorAll(".copyButton");
+const morseConsole = document.querySelector("#morseConsole");
+const light = document.querySelector(".light");
+const newChar = document.querySelector("#newChar");
 let startTime;
+let endTime;
 let spaceDown;
+let character;
+let unitTime = 130;
+
+function buttonDown() {
+    startTime = performance.now()
+    light.classList.add("green");
+    if (timeSinceLastButtonPress() > unitTime * 7) {
+        morseConsole.textContent += " /"
+    } else if (timeSinceLastButtonPress() > unitTime * 3) {
+        morseConsole.textContent += " "
+    }
+    keyButton.classList.add("mainBtnDown")
+    
+    light.textContent = "."
+    newChar.textContent = "."
+
+    setTimeout(function() {
+        if (keyButton.classList.contains("mainBtnDown")) {
+            light.textContent = "-"
+            newChar.textContent = "-"
+            character = "-"
+        }
+    }, unitTime * 3)
+}
+
+function buttonUp() {
+    endTime = performance.now()
+    light.classList.remove("green");
+    getTime();
+    morseConsole.textContent += `${ditOrDa()}`;
+    keyButton.classList.remove("mainBtnDown")
+    light.textContent = ""
+    newChar.textContent = ""
+}
+
+function getTime() {
+    console.log(parseFloat((endTime - startTime).toFixed(2)))
+}
+function timeSinceLastButtonPress() {
+    console.log("time since btn click:", parseFloat((startTime - endTime).toFixed(2)))
+    return (startTime - endTime)
+}
 
 keyButton.addEventListener("mousedown", function() {
-    startTime = performance.now();
-    keyButton.classList.add("mainBtnDown")
+    buttonDown();
 })
 keyButton.addEventListener("mouseleave", function() {
     if (keyButton.classList.contains("mainBtnDown")) {
-        console.log(performance.now() - startTime)
-        keyButton.classList.remove("mainBtnDown")
+        buttonUp();
     }
 })
 keyButton.addEventListener("mouseup", function() {
-    console.log(performance.now() - startTime)
-    keyButton.classList.remove("mainBtnDown")
+    buttonUp();
 })
 document.addEventListener("keydown", function(event) {
     if (event.code == "Space") {
@@ -26,16 +69,14 @@ document.addEventListener("keydown", function(event) {
         
         if ( !spaceDown) {
             spaceDown = true;
-            startTime = performance.now()
-            keyButton.classList.add("mainBtnDown")
+            buttonDown();
         }
     }
 })
 document.addEventListener("keyup", function(event) {
     if (event.code == "Space") {
         spaceDown = false;
-        console.log(performance.now() - startTime)
-        keyButton.classList.remove("mainBtnDown")
+        buttonUp();
     }
 })
 
@@ -64,6 +105,16 @@ copyButtons.forEach(button => {
         button.classList.remove("copyButtonDown")
     }));
 })
+
+function ditOrDa() {
+    const time = endTime - startTime
+    if (time < unitTime * 3 && character != "-") {
+        return "."
+    }
+    character = "";
+
+    return "-"
+}
   
 
 function appendCharacters(element, type) {
@@ -74,7 +125,6 @@ function appendCharacters(element, type) {
     element.forEach(__element => {
         charactersToAppend.forEach(character => {
             character = character.split(" | ")
-            console.log(character)
             __element.insertAdjacentHTML("beforeend", `<div class="character"><span class="characterText">${character[0]}</span><span class="morseText">${character[1]}</span></div>`)
         })
     })
